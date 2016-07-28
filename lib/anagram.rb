19cb48fb51b9f5c06962e15f96c90451b3f0bab0
Clifton Regis <github email address>
2016-07-28 09:43:29 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+ attr_accessor :word
+
+ def initialize(word)
+ 	@word = word
+ end
+
+ def match(array)
+    box = []
+    words = @word.chars.to_a.permutation.map(&:join)
+   #n =0
+   #until n > words.count 
+   array.each do |x|
+     words.each do |t|
+       if x == t
+        box << t
+       end
+     end
+  
+   end
+   return box.uniq
+ end
+
+end
+ 
+  # def all_possible_permutations
+  #   self.chars.to_a.permutation.map(&:join)
+  # end
